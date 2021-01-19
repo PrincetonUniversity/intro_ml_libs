ce4bc61c774e123e8a95e39535e71a876d33fb94
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-01-19 15:48:22 -0500

    Update install_jax_traverse.sh

--- jax/install_jax_traverse.sh
+++ jax/install_jax_traverse.sh
@@ -4,8 +4,8 @@
 # You may consider using a stable release instead of the master branch on github.
 
 module purge
-module load anaconda3/2020.7
-conda create --name jax-gpu python=3.7 numpy scipy cython six -y
+module load anaconda3/2020.11
+conda create --name jax-gpu python=3.8 numpy scipy cython six -y
 conda activate jax-gpu
 
 git clone https://github.com/google/jax
@@ -17,5 +17,5 @@ python build/build.py --enable_cuda \
                       --noenable_mkl_dnn \
                       --cuda_compute_capabilities 7.0 \
                       --bazel_path /usr/bin/bazel
-pip install -e build
+pip install dist/*.whl
 pip install -e .
