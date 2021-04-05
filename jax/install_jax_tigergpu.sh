60f132dad81c1d5b5a5254c12eae4413c52ce4cd
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-04-05 19:18:01 -0400

    Update install_jax_tigergpu.sh

--- jax/install_jax_tigergpu.sh
+++ jax/install_jax_tigergpu.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 module purge
 module load anaconda3/2020.11
-conda create --name jax-gpu python=3.8 numpy scipy cython six -y
+conda create --name jax-gpu python=3.8 numpy scipy six wheel -y
 conda activate jax-gpu
+
+export TMP=/tmp
 
 git clone https://github.com/google/jax
 cd jax
-module load cudatoolkit/10.2 cudnn/cuda-10.2/7.6.5 rh/devtoolset/8
+module load cudatoolkit/11.0 cudnn/cuda-11.0/8.0.2 rh/devtoolset/8
+
+# install jaxlib
 python build/build.py --enable_cuda  \
-                      --cudnn_path /usr/local/cudnn/cuda-10.2/7.6.5 \
+                      --cuda_path /usr/local/cuda-11.0 \
+                      --cudnn_path /usr/local/cudnn/cuda-11.0/8.0.2 \
                       --cuda_compute_capabilities 6.0 \
-                      --enable_march_native \
+                      --target_cpu_features native \
                       --enable_mkl_dnn
 pip install dist/*.whl
+
+# install jax
 pip install -e .
